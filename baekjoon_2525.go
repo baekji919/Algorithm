3eeaa187656f807f531fa43d6e5ed6b0bd317162
BaekJiHyun <jamie919@naver.com>
2022-04-12 23:04:32 +0900

    baekjoon 2525번 if문 오븐시계

---
+++ baekjoon_2525.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func Bj2525()  {
+
+	var a, b, c int
+
+	fmt.Scanln(&a, &b)
+	fmt.Scan(&c)
+
+	b = b + c
+
+	if b >= 60 {
+		a = a + b/60
+		b = b % 60
+		if a > 23 {
+			a = a - 24
+
+			fmt.Print(a, b)
+		} else {
+			fmt.Print(a, b)
+		}
+	} else {
+		fmt.Print(a, b)
+	}
+}
